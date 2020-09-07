cf93f59b84a869aa9e0de481e3437576e0a1bac5
shayde <shay.deri@gmail.com>
2020-09-07 10:19:49 +0300

    small refactor middleware

--- src/redux/middlewares/asyncRESTCall.js
+++ src/redux/middlewares/asyncRESTCall.js
@@ -8,23 +8,23 @@ import { makeLoadSuccessFailureActionType } from '../actions/actionTypes'
 const serverHost = getOr(null, '__myapp.server_host_url', window)
 
 const asyncRESTCall = store => next => action => {
+  const actionTypeTriple = makeLoadSuccessFailureActionType(action.type)
   if (getOr(undefined, 'payload.meta.type', action) === AsyncRESTMetaType) {
     const {dispatch} = store
     const route = getOr(null, 'payload.meta.route', action)
     if (route === null) {
-      failure(`action: ${action.type.loading}, missing route: ${JSON.stringify(action.payload)}`, dispatch, action.type)
+      successOrFailure(`action: ${action.type.loading}, missing route: ${JSON.stringify(action.payload)}`, dispatch, actionTypeTriple.failure)
     }
     const method = getOr(null, 'payload.meta.method', action)
     if (route === null) {
-      failure(`action: ${action.type.loading}, missing method (GET/POST...): ${JSON.stringify(action.payload)}`, dispatch, action.type)
+      successOrFailure(`action: ${action.type.loading}, missing method (GET/POST...): ${JSON.stringify(action.payload)}`, dispatch, actionTypeTriple.failure)
     }
     if (serverHost === null) {
-      failure(`action: ${action.type.loading}, missin host (from config.js), can't resolve path`, dispatch, action.type)
+      successOrFailure(`action: ${action.type.loading}, missin host (from config.js), can't resolve path`, dispatch, actionTypeTriple.failure)
     }
     const path = `${serverHost}${action.payload.meta.route}`
     const body = getOr(undefined, 'payload.body', action)
 
-    const actionTypeTriple = makeLoadSuccessFailureActionType(action.type)
     dispatch({
       type: actionTypeTriple.loading, ...omit('payload.meta.type', action)
     })
@@ -39,27 +39,20 @@ const asyncRESTCall = store => next => action => {
       },
       body: JSON.stringify(body)
     }).then((response) => {
-      success(response, dispatch, actionTypeTriple.success)
+      successOrFailure(response, dispatch, actionTypeTriple.success)
     }, (response) => {
-      failure(response, dispatch, actionTypeTriple.failure)
+      successOrFailure(response, dispatch, actionTypeTriple.failure)
     })
   }
   return next(action)
 }
 
-const success = (payload, dispatch, type) => {
+const successOrFailure = (payload, dispatch, type) => {
   payload.json().then((result) => {
     dispatch({
       type,
       payload: result
     })
-  })
-}
-
-const failure = (payload, dispatch, type) => {
-  dispatch({
-    type,
-    payload
   })
 }
 
