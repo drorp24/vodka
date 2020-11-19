03717b88869ee22dd1fb709b9ff7bc625adc816a
DROR POLIAK <DRORPOL@rf.local>
2020-11-19 19:06:40 +0200

    libraries, routing, login form

---
+++ src/components/common/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100); // fake async
+  },
+};
+
+export default function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return fakeAuth.isAuthenticated === true ? (
+          children
+        ) : (
+          <Redirect to='/login' />
+        );
+      }}
+    />
+  );
+}
