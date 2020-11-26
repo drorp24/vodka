2b159fed0522f3c306b7b7214145d1952209d9dd
DROR POLIAK <DRORPOL@rf.local>
2020-11-26 18:28:56 +0200

    semantic-ui'ing login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,37 +4,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchUser } from '../redux/reducers/usersReducer';
 import { useForm } from 'react-hook-form';
 
+import { Form, Button } from 'semantic-ui-react';
+import { Div } from './common/StyledElements';
+
 import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: '20vw',
+    margin: 'auto',
+    marginTop: '15vh',
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    width: '100%',
+    marginTop: '2vh !important',
   },
 }));
 
@@ -54,76 +37,48 @@ export default function Login() {
   }
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className={classes.form}
-          noValidate
-        >
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="user_name"
-            label="User name"
-            name="user_name"
-            autoComplete="user_name"
-            autoFocus
-            inputRef={register({ required: true })}
-            error={!!errors.user_name}
-            helperText="user_name is required"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            inputRef={register({ required: true })}
-            error={!!errors.password}
-            helperText="password is required"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}></Box>
-    </Container>
+    <Form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+      <Div
+        styleType="label1"
+        marginBottom="5vh"
+        display="flex"
+        justifyContent="center"
+      >
+        Sign in
+      </Div>
+      <Form.Input
+        name="user_name"
+        label="User name"
+        placeholder="User Name"
+        error={
+          errors.user_name
+            ? {
+                content: errors.user_name.message,
+                pointing: 'below',
+              }
+            : false
+        }
+      >
+        <input ref={register({ required: 'User name is required' })} />
+      </Form.Input>
+      <Form.Input
+        name="password"
+        label="Password"
+        placeholder="Password"
+        error={
+          errors.password
+            ? {
+                content: errors.password.message,
+              }
+            : false
+        }
+      >
+        <input ref={register({ required: 'Password is required' })} />
+      </Form.Input>
+
+      <Button type="submit" className={classes.submit}>
+        Sign in
+      </Button>
+    </Form>
   );
 }
